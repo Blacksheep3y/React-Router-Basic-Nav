56c5411403133217ed2cfb909a8782dd26cff748
Matthew Castillo <blacksheep3y@gmail.com>
2020-03-14 20:51:52 -0400

    Finished Navigation.js necessary file Changes to link files using react's SPA method

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { Route } from "react-router-dom";
 const App = () => (
   <div>
     <Navigation />
-    <Route exact path="/" component={About} />
-    <Route path="/about" component={Home} />
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
     <Route path="/contact" component={Contact} />
   </div>
 );
