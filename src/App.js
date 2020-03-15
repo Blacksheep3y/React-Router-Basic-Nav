4425209f6c61feef7b7dcf0faf8ca2b0dfd965e7
Matthew Castillo <blacksheep3y@gmail.com>
2020-03-14 20:44:14 -0400

    Created the Files, Initialized Project, Completed Steps for Implementing React Router, Completed Steps for Declaring your routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-
+import { Route } from "react-router-dom";
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/" component={About} />
+    <Route path="/about" component={Home} />
+    <Route path="/contact" component={Contact} />
   </div>
 );
 
